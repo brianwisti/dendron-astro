53ccc3fc234d8aceb8c7727c32b0669463643e28
Brian Wisti <brianwisti@pobox.com>
2023-03-09 09:37:13 -0800

    Issue/id-note-path-1 (#4)

---
+++ src/lib/DendronCache.ts
@@ -0,0 +1,27 @@
+import Path from "@mojojs/path";
+import type { NotesCache } from "@dendronhq/common-all";
+import YAML from "yaml";
+
+const defaultCachePath = new Path(
+  "C:/Users/brian/moa-dendron/notes/.dendron.cache.json"
+);
+
+export async function loadDendronCache(
+  path: Path = defaultCachePath
+): Promise<NotesCache> {
+  return YAML.parse((await path.readFile("utf-8")).toString()) as NotesCache;
+}
+
+export const DENDRON_CACHE = await loadDendronCache();
+
+export function getNoteHref(
+  fname: string,
+  dendronCache: NotesCache = DENDRON_CACHE
+) {
+  if (!Object.hasOwn(dendronCache.notes, fname)) {
+    return "#";
+  }
+
+  const noteId = dendronCache.notes[fname].data.id;
+  return `/notes/${noteId}`;
+}
