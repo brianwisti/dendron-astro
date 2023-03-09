e0ac3bcc5901490c300bcd243a55c37d03d9fde6
Brian Wisti <brianwisti@pobox.com>
2023-03-09 10:38:30 -0800

    Issue/use-relative-paths-2 (#5)

--- src/lib/DendronCache.ts
+++ src/lib/DendronCache.ts
@@ -1,27 +1,12 @@
 import Path from "@mojojs/path";
 import type { NotesCache } from "@dendronhq/common-all";
-import YAML from "yaml";
+import * as DENDRON_CACHE from "../notes/.dendron.cache.json";
 
-const defaultCachePath = new Path(
-  "C:/Users/brian/moa-dendron/notes/.dendron.cache.json"
-);
-
-export async function loadDendronCache(
-  path: Path = defaultCachePath
-): Promise<NotesCache> {
-  return YAML.parse((await path.readFile("utf-8")).toString()) as NotesCache;
-}
-
-export const DENDRON_CACHE = await loadDendronCache();
-
-export function getNoteHref(
-  fname: string,
-  dendronCache: NotesCache = DENDRON_CACHE
-) {
-  if (!Object.hasOwn(dendronCache.notes, fname)) {
+export function getNoteHref(fname: string) {
+  if (!Object.hasOwn(DENDRON_CACHE.notes, fname)) {
     return "#";
   }
 
-  const noteId = dendronCache.notes[fname].data.id;
+  const noteId = DENDRON_CACHE.notes[fname].data.id;
   return `/notes/${noteId}`;
 }
